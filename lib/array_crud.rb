0874de50224c72048046998e84860e3f11c6e676
Samanta Chowdhury <csamanta95@gmail.com>
2019-12-30 21:17:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
  p favorite_food= ["pizza", "pasta", "bread", "cheese"]
 end
 
-def add_element_to_end_of_array(array, element)
-  array << element
+def add_element_to_end_of_array(array, chicken)
+  array << chicken
 end
 
 def add_element_to_start_of_array(array, element)
