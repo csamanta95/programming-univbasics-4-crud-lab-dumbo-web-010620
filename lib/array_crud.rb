05864cfb18d04fb245f2c33a0983aa56a6b8575a
Samanta Chowdhury <csamanta95@gmail.com>
2019-12-30 21:18:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,8 +10,8 @@ def add_element_to_end_of_array(array, chicken)
   array << chicken
 end
 
-def add_element_to_start_of_array(array, element)
-
+def add_element_to_start_of_array(array, beef)
+  array.unshift(beef)
 end
 
 def remove_element_from_end_of_array(array)
